733457273dbb0ce642a5854251219e8fcea7ed2f
Tomas Rios <mune.thom2@gmail.com>
2021-11-17 01:13:49 -0300

    Array y Proyecto integrador

---
+++ Js/js.js
@@ -0,0 +1,34 @@
+let destinos = ["Mongolia", "Australia", "Argentina", "Brasil", "Africa", "italia", "Filipinas", "Cordoba, Argentina", "Salta, Argentina", "San juan, Argentina", "Marruecos, Africa", "Jujuy, Argentina"]
+
+function dondeir(){
+    alert ("¿No sabes donde ir? Bienvenido al sistema de ayuda para encontrar un destino")
+    let num = prompt("ingresa un destino del 0 al 12 y nosotros te recomendamos un destino al azar. Despues podes chequearlo en nuestra seccion: ''Galeria'' ")
+    alert("Podria interesarte viajar a....")
+    alert ( "¡" + destinos [num] + "!")
+
+}
+
+
+function reservar(){
+    alert("Bienvenido al sistema de reservas online de Myjourney.com")
+    let destiny = prompt("Especifique su destino de preferencia")
+    let tickets =  parseInt( prompt("Cantidad de pasajes deseados:") ) 
+    let adults = parseInt (prompt("Cantidad de pasajeros adultos:"))
+    let minors = parseInt (prompt ("Cantidad de pasajeros menores de edad:"))
+
+    let miviaje= new reserva (destiny, tickets, adults, minors);
+    miviaje.reservad();
+}
+
+class reserva{
+    constructor( destino, pasajes, adultos, menores){
+        this.destino= destino
+        this.pasajes = pasajes
+        this.adultos = adultos
+        this. menores = menores
+    }
+    
+    reservad(){
+        alert ("Se han reservado " + this.pasajes + " pasaje/s con destino a " + this.destino + ", " + this.adultos + " adultos y " + this.menores + " menores. ¡Disfrute su viaje!" )
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+MY JOURNEY
+
+Trabajo final del curso "Desarrollo Web" en Coder House por TOMÁS RÍOS
+
+My Journey es un sitio web de una empresa de viajes ficticia ubicada en Rosario, Argentina. Tiene como objetivo ayudar al usuario a planear su siguiente aventura, descubrir nuevos destinos y ponerse en contacto con la empresa.
+
+Podes ver a Myjourney en accion en el siguiente link:  https://thom-ri.github.io/My-journey/
+
+Para ejecutarlo solo es necesario clonar el repositorio y ejecutar la pagina index.html en una ventana de tu buscador.
+
+Quiero agradecer a mi profesor y mis tutores por la guia que me dieron para llevar a cabo este proyecto, y a mis padres por hacer posible esta oportunidad.
+
