3c1757d6adb1dd3b9d911db9b246ae9215525ea3
Tomas Rios <mune.thom2@gmail.com>
2021-12-29 22:49:00 -0300

    AJAX y Tercera entrega

--- Js/js.js
+++ Js/js.js
@@ -1,21 +1,19 @@
 let destinos = ["Mongolia", "Australia", "Argentina", "Brasil", "Africa", "italia", "Filipinas", "Cordoba, Argentina", "Salta, Argentina", "San juan, Argentina", "Marruecos, Africa", "Jujuy, Argentina"]
 
-$(".dontknowbtn").on("click", dondeir());
-
-function dondeir(){
+$(".discoverbtn").on("click", function(){
     let num = $("#minum").val() 
     alert("Podria interesarte viajar a....")
     alert ( "¡" + destinos [num] + "!")
-}
+});
 
 let destiny;
 let tickets; 
 let adults;
 let minors;
 let miviaje;
-let notificacionindex = document.getElementById("Notificationindex")
-let notificacionnacional= document.getElementById("Notificationnacional")
-let notificacioninter= document.getElementById("Notificationinter")
+let textoindex = document.getElementById("NotificacionIndexTexto")
+let textonacional= document.getElementById("NotificationNacionalTexto")
+let textointer= document.getElementById("Notificationinter")
 // RESERVAR
 function reservar(){
     class reserva{
@@ -27,7 +25,8 @@ function reservar(){
         }
         
         reservad(){
-            Notificationindex.innerHTML = "Se han reservado " + this.pasajes + " pasaje/s con destino a " + this.destino + ", " + this.adultos + " adultos y " + this.menores + " menores. ¡Disfrute su viaje!"
+            textoindex.innerHTML = "Se han reservado " + this.pasajes + " pasaje/s con destino a " + this.destino + ": " + this.adultos + " adulto/s y " + this.menores + " menor/es. ¡Disfrute su viaje!";
+            document.getElementById("notificacionindex").style.display = "block";            
         }
     }
     let destiny = document.getElementById("recipient-destiny").value
@@ -47,7 +46,8 @@ function reservarnacional(){
         }
         
         reservadnacional(){
-            Notificationnacional.innerHTML = "Se han reservado " + this.pasajes + " pasaje/s con destino a " + this.destino + ", " + this.adultos + " adultos y " + this.menores + " menores. ¡Disfrute su viaje!"
+            textonacional.innerHTML = "Se han reservado " + this.pasajes + " pasaje/s con destino a " + this.destino + ": " + this.adultos + " adulto/s y " + this.menores + " menor/es. ¡Disfrute su viaje!"
+            document.getElementById("notificacionnacional").style.display = "block";
         }
     }
     let destiny = document.getElementById("recipient-nacional-destiny").value
@@ -68,58 +68,57 @@ function reservarinter(){
         }
         
         reservadinter(){
-            notificacioninter.innerHTML = "Se han reservado " + this.pasajes + " pasaje/s con destino a " + this.destino + ", " + this.adultos + " adultos y " + this.menores + " menores. ¡Disfrute su viaje!"
+            textointer.innerHTML = "Se han reservado " + this.pasajes + " pasaje/s con destino a " + this.destino + ": " + this.adultos + " adulto/s y " + this.menores + " menor/es. ¡Disfrute su viaje!"
+            document.getElementById("notificacioninter").style.display = "block";
         }
     }
     
-    let destiny = document.getElementById("recipient-International-pasangers").value
+    let destiny = document.getElementById("recipient-Internacional-destiny").value
     let tickets = document.getElementById("recipient-International-pasangers").value
     let adults = document.getElementById("recipient-International-adults").value
     let minors = document.getElementById("recipient-International-children").value
     let miviaje= new reservainter (destiny, tickets, adults, minors);
     miviaje.reservadinter();                 
 }
+function cerrar(){
+    document.getElementById("notificacionindex").style.display = "none"; 
+}
+function cerrarinter(){
+    document.getElementById("notificacioninter").style.display = "none";
+}
+function cerrarnacional(){
+    document.getElementById("notificacionnacional").style.display = "none";
+}
+// Enviar formulario
+let urlpost = "https://jsonplaceholder.typicode.com/posts"
+let ContactRequest = {
+    name : $("#unom").val(),
+    phone: $("#unum").val(),
+    email: $("#exampleInputEmail1").val(),
+    subscription: $("#exampleCheck1").val(), 
+    coment : $("#ucon").val(),
+}
+$("#Postbtn").click(function(e){
+    e.preventDefault(); 
+    $.post(urlpost,ContactRequest,(respuesta,estado) =>{
+        if(estado === "success"){
+            $("#NotificationContacto").html("Tu consulta ha sido enviada exitosamente")
+            $("#contacttoast").toast("show")  
+        }  
 
-
-
-
-
+    })
+})
 // COMENTAR
 let comentarios = document.getElementById("seccioncomentarios")
-function comentar(){
-    let Nombre = document.getElementById("recipient-name").value
-    let comentario = document.getElementById("message-text").value
-    let parrafo = document.createElement("p")
-    parrafo.innerHTML = Nombre + " dice: " + comentario ;
-    document.body.appendChild(parrafo);
-}
-
-
-// EMPEZAR VIAJE
-
-let divmiviaje = document.getElementById("miviajenacional")
-
-
-
-function empezarviaje(){
-    let partidanacional = document.getElementById("Fida").value;
-    let regresonacional = document.getElementById("Fre").value;
-    let numpasajeros = document.getElementById("npasa").value;
-    Crear()
-}
-
-
-
-function Crear(){
-    let inputpartida = document.createElement("input").setAttribute("id", "partida").setAttribute("value", partidanacional)
-    let inputregreso = document.createElement("input").setAttribute("id", "regreso").setAttribute("value", regresonacional)
-    let inputpasajeros = document.createElement("input").setAttribute("id", "pasajeros").setAttribute("value", numpasajeros)
-    document.divmiviaje.appendChild(inputpartida)
-    document.divmiviaje.appendChild(inputregreso)
-
-}
-
-// function calcular(){
-//     document.getElementById("partidanacional").value = document.getElementById("fida").value 
-//     document.getElementById("regresonacional").value = document.getElementById("fre").value 
-// }
+$("#btnsendmsg").click(function(){
+    let Nombre = $("#recipient-name").val()
+    let comentario = $("#message-text").val()
+    $("#seccioncomentarios").html(`
+                                    <div class="card">
+                                        <div class="card-header">`+ Nombre + `</div>
+                                        <div class="card-body">
+                                            <p class="card-text">`+ comentario +`</p>
+                                        </div>
+                                    </div>
+    `)
+})
